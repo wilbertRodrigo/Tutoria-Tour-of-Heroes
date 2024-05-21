7ef43b8dc0f64f64f7f3463a608dfc9cd35b03ab
wilbertRodrigo <wilbert.rodrigo@pulsar-sd.com>
2024-05-22 03:42:57 +0800

    continuing tour-of-heroes-tutorial

--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -1,13 +1,13 @@
 import { Hero } from './hero';
 
 export const HEROES: Hero[] = [
-  { id: 12, name: 'Dr. Nice' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr. IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' },
+  { id: 12, name: 'Dr. Nice', skill: 'charge' },
+  { id: 13, name: 'Bombasto', skill: 'bloodlust' },
+  { id: 14, name: 'Celeritas', skill: 'Blind' },
+  { id: 15, name: 'Magneta', skill: 'Invisibility' },
+  { id: 16, name: 'RubberMan', skill: 'Frenzy Curse' },
+  { id: 17, name: 'Dynama', skill: 'Ice ring' },
+  { id: 18, name: 'Dr. IQ', skill: 'implode' },
+  { id: 19, name: 'Magma', skill: 'Chain lightning' },
+  { id: 20, name: 'Tornado', skill: 'Blow away' },
 ];
